560faf2ff01e7cb5054122fd9ef3b593eafa9377
Suyash Singh <suyxcode@gmail.com>
2026-01-11 20:38:09 +0530

    Update and rename 0550-game-play-analysis-iv.sql to Solution.sql

---
+++ SQL_50_Questions/0550-game-play-analysis-iv/Solution.sql
@@ -0,0 +1,6 @@
+select 
+round(sum(player_login)/ count(distinct player_id),2) as fraction  
+from 
+(select player_id ,
+datediff(event_date, min(event_date) over(partition by player_id ))=1 as player_login
+from activity) as new_table
