1d6631ea31cf9085a6687059323038414f6465ab
Suyash Singh <suyxcode@gmail.com>
2026-01-11 20:38:42 +0530

    Runtime 678 ms  | Beats 16.61%

--- SQL_50_Questions/0550-game-play-analysis-iv/Solution.sql
+++ SQL_50_Questions/0550-game-play-analysis-iv/Solution.sql
@@ -1,5 +1,4 @@
-select 
-round(sum(player_login)/ count(distinct player_id),2) as fraction  
+select round(sum(player_login)/ count(distinct player_id),2) as fraction  
 from 
 (select player_id ,
 datediff(event_date, min(event_date) over(partition by player_id ))=1 as player_login
