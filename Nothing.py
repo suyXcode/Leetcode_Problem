e71770ac145fa0c09d428c28e4a8b0f0e01baa01
Suyash Singh <suyxcode@gmail.com>
2026-03-13 10:00:18 +0530

    Fix capitalization in print statements
    Corrected capitalization in print statements.

--- Nothing.py
+++ Nothing.py
@@ -1,13 +1,10 @@
-print("hello leetcode")
+print("Hello Leetcode")
 print("Nothing")
 print("Namste Python")
-print("Namste Suyash ")
-print("Hello Suyxcode")
-print("namste python")
 print("Namste Sql")
 print("Namste Maths")
 for i in range(1,11):
   print(i)
 
-print("the python programming!!")
+print("The python programming!!")
 print("The SQL ")
