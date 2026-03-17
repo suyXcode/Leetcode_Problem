be05c997a3f8346cc5e98a72c5886df3c3a53e65
Suyash Singh <suyxcode@gmail.com>
2026-03-18 03:41:44 +0530

    Refactor print statements in Nothing.py

--- Nothing.py
+++ Nothing.py
@@ -1,11 +1,9 @@
 print("Hello Leetcode")
-print("Nothing")
-print("Namste Python")
-print("Namste Sql")
-print("Namste Maths")
-for i in range(1,11):
-  print(i)
-
 print("The python programming!!")
 print("The SQL ")
 print("The Leetcode")
+print("Nothing")
+for i in range(1,11):
+  print(i)
+
+
