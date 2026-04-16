6fbda08739fa3151f4ad70be98487abc9be830ea
Suyash Singh <suyxcode@gmail.com>
2026-04-16 23:38:26 +0530

    Update 049anagrams.py

--- strings/049anagrams.py
+++ strings/049anagrams.py
@@ -1,8 +1,6 @@
 # 49. Group Anagrams
 # Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
- 
-
 # Example 1:
 
 # Input: strs = ["eat","tea","tan","ate","nat","bat"]
@@ -26,7 +24,6 @@
 
 # Output: [["a"]]
 
- 
 
 # Constraints:
 
