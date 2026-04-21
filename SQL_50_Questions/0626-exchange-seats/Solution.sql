b5918702e1264af5f0993d7d2ab7773e166847a2
Suyash Singh <suyxcode@gmail.com>
2026-04-21 19:47:44 +0530

    Update Solution.sql

--- SQL_50_Questions/0626-exchange-seats/Solution.sql
+++ SQL_50_Questions/0626-exchange-seats/Solution.sql
@@ -3,6 +3,4 @@ select
 case 
 when id = (select max(id) from seat) and id % 2=1 then id
 when id % 2=1 then id+1 else id-1 end as id,
-student
-from seat
-order by id ;
+student from seat order by id ;
