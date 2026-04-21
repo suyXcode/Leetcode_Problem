44c5f1431d9dc65dc806a3655247703bc9f5d398
Suyash Singh <suyxcode@gmail.com>
2026-04-21 19:48:02 +0530

    Update Solution.sql

--- SQL_50_Questions/0626-exchange-seats/Solution.sql
+++ SQL_50_Questions/0626-exchange-seats/Solution.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
-select
-case 
-when id = (select max(id) from seat) and id % 2=1 then id
+select case when id = (select max(id) from seat) and id % 2=1 then id
 when id % 2=1 then id+1 else id-1 end as id,
 student from seat order by id ;
