cc6611bd1ae71c6dd535c45e4fba18e3416ac345
Suyash Singh <suyxcode@gmail.com>
2026-05-22 22:29:13 -0400

    Update Nothing.py

--- Nothing.py
+++ Nothing.py
@@ -6,8 +6,4 @@ print("The Leetcode")
 print("Nothing")
 print("The commit")
 print("This repo has include all type of questions which have Leetcode Problems!")
-print("---------------------------")
-print("Exam")
-print("---------------------------")
-print("9 may 2026 Cloud Computing")
-print("Completed"
+
