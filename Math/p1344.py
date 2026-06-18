b28a10a2635c03015856c3570248ee6c2487d972
Suyash Singh <suyxcode@gmail.com>
2026-06-18 14:03:50 +0530

    1344. Angle Between Hands of a Clock

---
+++ Math/p1344.py
@@ -0,0 +1,8 @@
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+        hour_angle = (hour % 12) * 30 + minutes * 0.5
+        minute_angle = minutes * 6
+
+        angle = abs(hour_angle - minute_angle)
+
+        return min(angle, 360 - angle)
