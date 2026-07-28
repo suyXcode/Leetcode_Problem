8784adc2817cb8ae4dd8905d118122e4a4309d7c
Suyash Singh <suyxcode@gmail.com>
2026-07-28 16:18:33 +0530

    3517. Smallest Palindromic Rearrangement I

---
+++ strings/p3517.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        cnt = Counter(s)
+
+        left = []
+        mid = ""
+
+        for ch in sorted(cnt):
+            left.append(ch * (cnt[ch] // 2))
+            if cnt[ch] % 2:
+                mid = ch
+
+        left = "".join(left)
+        return left + mid + left[::-1]
