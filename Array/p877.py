48a5f9950ea4a285a3d79058262c3fa95b56cb55
Suyash Singh <suyxcode@gmail.com>
2026-08-02 22:12:16 +0530

    877. Stone Game
    Medium

---
+++ Array/p877.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        n = len(piles)
+        dp = piles[:]
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i] = max(piles[i] - dp[i + 1], piles[j] - dp[i])
+
+        return dp[0] > 0
