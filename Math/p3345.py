7301bb55888403534eea73a0ff0b4e3ade8c9479
Suyash Singh <suyxcode@gmail.com>
2026-08-06 08:58:32 +0530

    3345. Smallest Divisible Digit Product I

---
+++ Math/p3345.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        def digit_product(x):
+            prod = 1
+            while x > 0:
+                prod *= x % 10
+                x //= 10
+            return prod
+
+        while True:
+            if digit_product(n) % t == 0:
+                return n
+            n += 1
