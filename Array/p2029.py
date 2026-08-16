bb9544a05029ab5ecb0c30ad6cdcb561ee64753e
Suyash Singh <suyxcode@gmail.com>
2026-08-16 08:24:23 +0530

    2029. Stone Game IX

---
+++ Array/p2029.py
@@ -0,0 +1,13 @@
+class Solution:
+    def stoneGameIX(self, stones):
+        cnt = [0, 0, 0]
+
+        for x in stones:
+            cnt[x % 3] += 1
+
+        # Stones divisible by 3 don't change the modulo,
+        # but they change the number of turns.
+        if cnt[0] % 2 == 0:
+            return cnt[1] > 0 and cnt[2] > 0
+
+        return abs(cnt[1] - cnt[2]) > 2
