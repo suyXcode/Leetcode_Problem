af21af064681e1faf9a5e58114484bdeb0647b44
Suyash Singh <suyxcode@gmail.com>
2026-08-18 10:15:09 +0530

    Create p3517.py

---
+++ Array/p3517.py
@@ -0,0 +1,19 @@
+class Solution:
+    def largestInteger(self, nums: list[int], k: int) -> int:
+        count = {}
+
+        n = len(nums)
+
+        for i in range(n - k + 1):
+            window = set(nums[i:i + k])
+
+            for x in window:
+                count[x] = count.get(x, 0) + 1
+
+        ans = -1
+
+        for x, freq in count.items():
+            if freq == 1:
+                ans = max(ans, x)
+
+        return ans
