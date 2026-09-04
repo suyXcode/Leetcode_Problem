7be40437d16d74645adecefe54cf30373f04c553
Suyash Singh <suyxcode@gmail.com>
2026-09-04 14:59:17 +0530

    3903. Smallest Stable Index I

---
+++ Array/p3903.py
@@ -0,0 +1,24 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+
+        # prefix maximum
+        prefix_max = [0] * n
+        prefix_max[0] = nums[0]
+
+        for i in range(1, n):
+            prefix_max[i] = max(prefix_max[i - 1], nums[i])
+
+        # suffix minimum
+        suffix_min = [0] * n
+        suffix_min[n - 1] = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(suffix_min[i + 1], nums[i])
+
+        # Find the first stable index
+        for i in range(n):
+            if prefix_max[i] - suffix_min[i] <= k:
+                return i
+
+        return -1
